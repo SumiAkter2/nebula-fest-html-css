0b68e86df629a84dc82daa38c1f44a2ccbe865af
SumiAkter2 <akteralisha36@gmail.com>
2025-01-19 17:06:54 +0600

    alhamdulillah card slider done

--- js/imageSlider.js
+++ js/imageSlider.js
@@ -1,29 +1,29 @@
-(function() {
-    function controls() {
-      this.className += ' selected';
-      var siblings = this.parentNode.children;
-      for (var i = 0; i < siblings.length; i++) {
-        if (siblings[i] !== this) {
-          siblings[i].className = siblings[i].className.replace(' selected', '');
-        }
-      }
+// (function() {
+//     function controls() {
+//       this.className += ' selected';
+//       var siblings = this.parentNode.children;
+//       for (var i = 0; i < siblings.length; i++) {
+//         if (siblings[i] !== this) {
+//           siblings[i].className = siblings[i].className.replace(' selected', '');
+//         }
+//       }
   
-      var containerHeight = document.querySelector('.container').offsetHeight;
-      var slideDiv = document.querySelector('.slideDiv');
-      var index = Array.prototype.indexOf.call(this.parentNode.children, this);
+//       var containerHeight = document.querySelector('.container').offsetHeight;
+//       var slideDiv = document.querySelector('.slideDiv');
+//       var index = Array.prototype.indexOf.call(this.parentNode.children, this);
   
-      slideDiv.style.transition = 'top 0.45s';
-      slideDiv.style.top = '-' + (containerHeight * index) + 'px';
-    }
+//       slideDiv.style.transition = 'top 0.45s';
+//       slideDiv.style.top = '-' + (containerHeight * index) + 'px';
+//     }
   
-    function runCode() {
-      var buttons = document.querySelectorAll('.button');
-      for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', controls, false);
-      }
-    }
+//     function runCode() {
+//       var buttons = document.querySelectorAll('.button');
+//       for (var i = 0; i < buttons.length; i++) {
+//         buttons[i].addEventListener('click', controls, false);
+//       }
+//     }
   
-    runCode();
-  })();
+//     runCode();
+//   })();
   
   
